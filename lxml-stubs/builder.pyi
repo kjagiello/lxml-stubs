62969e57f7f2dfde852a94eefb72eb3ba6057269
Abel Cheung <abelcheung@gmail.com>
2021-03-13 21:58:31 +0000

    Impl lxml.builder and simple test cases

---
+++ lxml-stubs/builder.pyi
@@ -0,0 +1,33 @@
+from typing import Any, Callable, Mapping, Optional, Type, TypeVar
+
+from ._types import _AnyStr
+from .etree import _Element
+
+_T = TypeVar("_T")
+
+class ElementMaker:
+    def __init__(
+        self,
+        typemap: Optional[Mapping[Type[_T], Callable[[_Element, _T], None]]] = ...,
+        namespace: Optional[str] = ...,
+        nsmap: Optional[Mapping[Optional[_AnyStr], _AnyStr]] = ...,
+        # makeelement is callable of same arg as etree.Element()
+        makeelement: Optional[Callable[..., _Element]] = ...,
+    ) -> None: ...
+    def __call__(
+        self,
+        tag: str,
+        # Although default ElementMaker only accepts _Element and types
+        # interpretable by default typemap (that is str, CDATA and dict)
+        # as children, typemap can be expanded to make sure item of any
+        # type is accepted. So it can't be typed.
+        *children: Any,
+        **attrib: _AnyStr,
+    ) -> _Element: ...
+    # __getattr__ here is special. ElementMaker is a factory that generates
+    # element of *any* tag, as long as tag name does not conflict with basic
+    # object methods (yes, including python keywords like "class" and "for",
+    # which are common in HTML)
+    def __getattr__(self, name: str) -> Callable[..., _Element]: ...
+
+E: ElementMaker = ...
